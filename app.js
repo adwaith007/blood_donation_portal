b8a6e4f57a72ae24e49afc6916c4b628ee2c07ad
shakti97 <shaktisingh230797@gmail.com>
2019-12-14 17:07:25 +0530

    added oauth support

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const path = require('path');
-const engine = require('ejs-locals');
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const path = require("path");
+const engine = require("ejs-locals");
+const authRoutes = require("./app/routes/auth");
+const profileRoutes = require("./app/routes/profile");
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const config = require('./config');
-const routes = require('./app/routes');
+const config = require("./config");
+const routes = require("./app/routes");
 
-mongoose.connect(config.dbURI);
-
-
-
+mongoose.connect(config.dbURI, () => {
+  console.log("connected to mongodb");
+});
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //path
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   session({
     secret: config.sessionSecret,
     resave: false,
     saveUninitialized: true,
-  }),
+  })
 );
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
   if (req.user) {
@@ -41,18 +44,20 @@ app.use((req, res, next) => {
   return next();
 });
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  limit: '30mb',
-  extended: true,
-}));
+app.use(
+  bodyParser.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
 
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 app.use(routes);
-
 //routes
-app.get("/",(req,res)=>{
-   res.render("login.ejs");
+app.get("/", (req, res) => {
+  res.render("login.ejs");
 });
-
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
--- config/config.example.js
+++ config/config.example.js
@@ -1,7 +1,10 @@
 module.exports = {
-    appBaseURL: 'http://localhost:3000',
-    port: 3000,
-    sessionSecret: 'Insert some random secret string here',
-    dbURI: 'Insert your MongoDB URI here', // Like, mongodb://localhost/blooddonation
-  };
-  
+  appBaseURL: "http://localhost:3000",
+  port: 3000,
+  sessionSecret: "Insert some random secret string here",
+  dbURI: "Insert your MongoDB URI here", // Like, mongodb://localhost/blooddonation
+  google: {
+    clientID: "Insert Client Id here",
+    clientSecret: "Insert Client Secret Here"
+  }
+};
