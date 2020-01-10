1306913c1b6ca287d7ab9d94906f5726bfe506c7
Akhil Seshan <akhilses@gmail.com>
2020-01-10 08:57:40 +0530

    routes and authentication using passport.js

---
+++ app/routes/profile.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+//const users = require('./models/users');
+const mongoose = require("mongoose");
+const Request = require("../../models/request");
+const bodyParser = require("body-parser");
+
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/google");
+  } else {
+    next();
+  }
+};
+
+router.get("/", authCheck, (req, res) => {
+  console.log(req.user);
+  Request.find({ $or: [{ _id: req.user._id }] }).then(currentRequest => {
+    if (currentRequest) {
+      var details = {
+        user_details: req.user,
+        request_details: currentRequest,
+        no_of_requests: currentRequest.length
+      };
+      console.log(details);
+      res.render("profile", { user: details });
+    }
+  });
+});
+
+router.post("/request/cancel", (req, res) => {
+  var status = JSON.parse(req.body.cancel);
+  console.log(req.body.status);
+  Request.updateOne(
+    { _id: status.id },
+    { $set: { requestStatus: "cancelled" } },
+    (err, res) => {
+      console.log("updated successfully");
+    }
+  );
+  res.redirect("/profile");
+});
+
+module.exports = router;
